b5757cae2e556a7480d171664a9158b06704d8a6
Iker Iturralde <ikerite02@gmail.com>
2022-12-21 13:39:03 +0100

    Splash texto

--- app/src/main/java/study/project/activities/SplashActivity.java
+++ app/src/main/java/study/project/activities/SplashActivity.java
@@ -26,16 +26,26 @@ public class SplashActivity extends AppCompatActivity {
         openApp();
         texto = findViewById(R.id.health);
 
-        ImageView mfondo = findViewById(R.id.fondo);
-        Glide.with(this)
-                .load(R.drawable.vitals)
-                .transition(DrawableTransitionOptions.withCrossFade(100))
-                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.purple_200)))
-                .into(mfondo);
-        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.fadein);
-        mfondo.startAnimation(myanim);
+        ImageView vital = findViewById(R.id.vital);
 
-        Animation myanim2 = AnimationUtils.loadAnimation(this, R.anim.desaparecer);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.blink);
+        vital.startAnimation(myanim);
+
+
+        //    Glide for loading girls
+        ImageView mSea = findViewById(R.id.fondo);
+
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1627842467534-7a5d24b6fd89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80")
+//                .load(R.drawable.girl)
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .centerCrop()
+//                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.bluex)))
+//                .circleCrop()
+                .into(mSea);
+
+        Animation myanim2 = AnimationUtils.loadAnimation(this, R.anim.fadein);
         texto.startAnimation(myanim2);
 
 
@@ -51,7 +61,7 @@ public class SplashActivity extends AppCompatActivity {
                         .this, MainActivity.class);
                 startActivity(intent);
             }
-        }, 5000);
+        }, 10000);
 
 
     }
