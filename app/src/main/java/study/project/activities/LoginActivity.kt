5f66cb58ed3f3becd9226108cbfcd9036646fab5
Joaquin Ayllon <joaquin.ayllon@optivamedia.com>
2022-12-21 10:58:18 +0100

    login changes

---
+++ app/src/main/java/study/project/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package study.project.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import study.project.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
