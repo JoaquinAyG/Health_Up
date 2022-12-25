24704eec65e2a48ba53ea36d502f6b6c7432aaeb
Joaquin Ayllon Garcia <90848989+JoaquinAyG@users.noreply.github.com>
2022-12-25 14:44:29 +0100

    Feature/login (#10)
    * rounded up buttons and databinding refactor
    
    * reformat files

--- app/src/main/java/study/project/activities/MainActivity.kt
+++ app/src/main/java/study/project/activities/MainActivity.kt
@@ -4,14 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import study.project.R
-import study.project.databinding.ActivityLoginBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
     }
 }
