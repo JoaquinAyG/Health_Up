a3e4e936b2db25090815df6da037dfb6777f3cab
JoaquinAyG <joaquin.ayllon@educa.madrid.org>
2023-01-06 23:26:13 +0100

    Rename var

--- app/src/main/java/study/project/activities/LoginActivity.kt
+++ app/src/main/java/study/project/activities/LoginActivity.kt
@@ -15,7 +15,7 @@ class LoginActivity : AppCompatActivity() {
 
         binding.apply {
 
-            btnlogin.setOnClickListener {
+            btnLogin.setOnClickListener {
                 val username = etUsername.text.toString()
                 val password = etPassword.text.toString()
                 if (Utils.validateCredentials(username, password)) {
@@ -31,7 +31,7 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
 
-            signup.setOnClickListener {
+            tvSignup.setOnClickListener {
                 Toast.makeText(this@LoginActivity, "Sign Up", Toast.LENGTH_SHORT).show()
 //            val intent = Intent(this, SignupActivity::class.java)
 //            startActivity(intent)
