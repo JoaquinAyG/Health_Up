14b8e3b695102dc1958568a4aeb349d50c595983
Jordan Chimborazo <jordanchimborazo10@gmail.com>
2023-01-16 12:16:09 +0100

    finish_check

--- app/src/main/java/study/project/activities/RegisterActivity.java
+++ app/src/main/java/study/project/activities/RegisterActivity.java
@@ -1,15 +1,17 @@
-package study.project;
+package study.project.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+public class RegisterActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
+
 
     }
 }
