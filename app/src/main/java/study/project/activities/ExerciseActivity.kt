b43d962446ec62a3406252131a8da77f34bc809a
KoltX <100805491+KoltX@users.noreply.github.com>
2023-01-19 09:50:17 +0100

    model activity exercise (#27)
    Co-authored-by: alvaro <ikerite02@gmail.com>

---
+++ app/src/main/java/study/project/activities/ExerciseActivity.kt
@@ -0,0 +1,13 @@
+package study.project.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import study.project.R
+
+
+class ExerciseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise)
+    }
+}
