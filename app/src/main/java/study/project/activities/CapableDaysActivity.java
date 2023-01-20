fb1cf98edfb105ee74ea3c96aa382ba6703e582c
Jordan <59701530+Jordan1407@users.noreply.github.com>
2023-01-20 09:51:28 +0100

    Feature/capable days (#33)
    * finish_check
    
    * finish_capable_days.xml
    
    * capabledays fragment

---
+++ app/src/main/java/study/project/activities/CapableDaysActivity.java
@@ -0,0 +1,16 @@
+package study.project.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import study.project.R;
+
+public class CapableDaysActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_capable_days);
+    }
+}
