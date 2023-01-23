de4c7fe217237b8329853dd7f890e4ffcbf29b25
alvaro <ikerite02@gmail.com>
2023-01-23 12:13:32 +0100

    Profile activity

--- app/src/main/java/study/project/ui/profile/ProfileFragment.kt
+++ app/src/main/java/study/project/ui/profile/ProfileFragment.kt
@@ -29,10 +29,7 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textProfile
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
     }
 
