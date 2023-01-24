37344e5a15a81a6a3423b92225d3f42b6dcb9a8a
Jordan Chimborazo <jordanchimborazo10@gmail.com>
2023-01-24 12:57:01 +0100

    finish fragment_password.xml

---
+++ app/src/main/java/study/project/fragments/FragmentPasswordActivity.java
@@ -0,0 +1,20 @@
+package study.project.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import study.project.R;
+
+public class FragmentPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_password);
+
+    }
+
+}
