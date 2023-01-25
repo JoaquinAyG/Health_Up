3d3c1152718470f5239ce2497a0848c4292512f7
JoaquinAyG <joaquin.ayllon@educa.madrid.org>
2023-01-25 14:54:46 +0100

    Strings and background for onboarding

--- app/src/main/java/study/project/activities/OnBoardingActivity.kt
+++ app/src/main/java/study/project/activities/OnBoardingActivity.kt
@@ -2,7 +2,6 @@ package study.project.activities
 
 import android.app.ActionBar.LayoutParams
 import android.app.AlertDialog
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.os.Process
@@ -18,26 +17,31 @@ import study.project.adapters.OnBoardingViewPagerAdapter
 
 class OnBoardingActivity : AppCompatActivity() {
 
-    lateinit var llSliderDots: LinearLayout
-    lateinit var btnContinue: Button
-    lateinit var btnFinish: Button
+    private lateinit var llSliderDots: LinearLayout
+    private lateinit var btnContinue: Button
+    private lateinit var btnFinish: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding)
         //no action bar
-        supportActionBar?.hide();
+        supportActionBar?.hide()
 
-        val vpOnboarding = findViewById<ViewPager>(R.id.vp_onboarding)
+        val vpOnboard = findViewById<ViewPager>(R.id.vp_onboarding)
         btnContinue = findViewById(R.id.btn_continue)
         btnFinish = findViewById(R.id.btn_finish)
         llSliderDots = findViewById(R.id.llSliderDots)
 
-        val pages = arrayListOf("Page 1", "Page 2", "Page 3", "Page 4")
+        val pages = arrayListOf(
+            resources.getStringArray(R.array.onboarding_welcome).joinToString("\n \n"),
+            resources.getStringArray(R.array.onboarding_terms_and_conditions).joinToString("\n \n"),
+            resources.getStringArray(R.array.onboarding_usage).joinToString("\n \n"),
+            resources.getStringArray(R.array.onboarding_authors).joinToString("\n \n"),
+        )
 
         val adapter = OnBoardingViewPagerAdapter(this@OnBoardingActivity, pages)
-        vpOnboarding.adapter = adapter
+        vpOnboard.adapter = adapter
 
-        vpOnboarding.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+        vpOnboard.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrolled(
                 position: Int,
                 positionOffset: Float,
@@ -59,10 +63,10 @@ class OnBoardingActivity : AppCompatActivity() {
         setDots(pages.size, 0)
 
         btnContinue.setOnClickListener {
-            vpOnboarding.currentItem = vpOnboarding.currentItem + 1
+            vpOnboard.currentItem = vpOnboard.currentItem + 1
         }
 
-        btnFinish.setOnClickListener{
+        btnFinish.setOnClickListener {
             startActivity(Intent(this@OnBoardingActivity, LoginActivity::class.java))
             finish()
         }
@@ -80,7 +84,7 @@ class OnBoardingActivity : AppCompatActivity() {
         }
     }
 
-    private fun setDots(size: Int, pos: Int){
+    private fun setDots(size: Int, pos: Int) {
         llSliderDots.removeAllViews()
         for (i in 0 until size) {
             val iv = ImageView(this)
@@ -89,13 +93,14 @@ class OnBoardingActivity : AppCompatActivity() {
             } else {
                 iv.setImageResource(R.drawable.dot_empty)
             }
-            val params = LayoutParams(40, 40)
+            val params = LayoutParams(20, 20)
             if (i != 0)
                 params.marginStart = 80
             iv.layoutParams = params
             llSliderDots.addView(iv)
         }
     }
+
     private fun cerrarAplicacion() {
         AlertDialog.Builder(this)
             .setIcon(R.drawable.logo)
@@ -103,12 +108,13 @@ class OnBoardingActivity : AppCompatActivity() {
             .setCancelable(false)
             .setNegativeButton("Cancel", null)
             .setPositiveButton(
-                "exit",
-                DialogInterface.OnClickListener { dialog, which ->
-
-                    Process.killProcess(Process.myPid())   }).show()
+                "exit"
+            ) { _, _ ->
+                Process.killProcess(Process.myPid())
+            }.show()
 
     }
+
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
             cerrarAplicacion()
