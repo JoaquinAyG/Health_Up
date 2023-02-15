e96a894f3bb86cbec1e1e82b25758aa92acd79c0
kino <joaquin.ayllongar@gmail.com>
2023-02-15 09:01:17 -0800

    fav Room Database

--- app/src/main/java/study/project/HealthUpApplication.kt
+++ app/src/main/java/study/project/HealthUpApplication.kt
@@ -3,11 +3,15 @@ package study.project
 import android.app.Application
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import study.project.databases.FavRoomDatabase
 import study.project.databases.UserRoomDatabase
+import study.project.repos.FavRepository
 import study.project.repos.UserRepository
 
 class HealthUpApplication : Application() {
     val applicationScope = CoroutineScope(SupervisorJob())
-    val database by lazy { UserRoomDatabase.getDatabase(this, applicationScope) }
-    val repository by lazy { UserRepository(database.userDao()) }
+    val userDatabase by lazy { UserRoomDatabase.getDatabase(this, applicationScope) }
+    val userRepository by lazy { UserRepository(userDatabase.userDao()) }
+    val favDatabase by lazy { FavRoomDatabase.getDatabase(this, applicationScope) }
+    val favRepository by lazy { FavRepository(favDatabase.favDao()) }
 }
---
+++ app/src/main/java/study/project/dao/FavDao.kt
@@ -0,0 +1,23 @@
+package study.project.dao
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+import study.project.models.Fav
+
+@Dao
+interface FavDao {
+    @Query("SELECT * FROM fav_table WHERE id=:id ")
+    fun loadSingle(id: String): Flow<Fav>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(fav: Fav)
+
+    @Query("DELETE FROM fav_table")
+    fun deleteAll()
+
+    @Delete
+    fun delete(model: Fav)
+
+    @Update
+    suspend fun update(fav: Fav)
+}
